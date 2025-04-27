5d141265efe9a334b4c5fd5a86704dbad68d064d
William Chapman <William.Chapman@coxautoinc.com>
2025-04-27 13:44:44 -0400

    Initial commit

---
+++ src/main/java/com/pubsub/salesforcepubsubapispringboot/SalesforcePubSubApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.pubsub.salesforcepubsubapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesforcePubSubApiSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesforcePubSubApiSpringbootApplication.class, args);
+    }
+
+}
